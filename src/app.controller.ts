3f7046f3409efac818f7801f09db47dc26cdf040
Alexandre Queiroz <alexandre.goncalves33@outlook.com>
2022-07-10 17:50:02 -0300

    create microservice

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,19 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller } from '@nestjs/common';
+import { EventPattern, MessagePattern, Payload } from '@nestjs/microservices';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  // @EventPattern('storage-ec')
+  // getStorage(@Payload() message) {
+  //   console.log('Message Received', message);
+  // }
+
+
+  @MessagePattern('storage-ec')
+  getStorage(@Payload() message) {
+    console.log('Message Received', message);
   }
 }
